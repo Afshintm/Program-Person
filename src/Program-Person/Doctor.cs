94cd615f113e0540a2789cec74e66355efa92f86
Afshin Teymoori <afshin_tm@yahoo.com>
2022-03-10 11:58:19 +1100

    add sql

--- src/Program-Person/Doctor.cs
+++ src/Program-Person/Doctor.cs
@@ -1,6 +1,6 @@
 ﻿namespace Program_Person
 {
-    public class Doctor: Person
+    public class Doctor_old: Person
     {
        
         public Specialities Speciality { get; set; }
@@ -15,6 +15,34 @@
         {
             //return base.ToString();
             return "I am doctor " + Name + " " + Family + " and " + Age.ToString()+" years old, my specilality is " + Speciality;
+        }
+    }
+
+    public class Doctor
+    {
+        public Doctor(Person p) 
+        { 
+            Person = p; 
+        }
+        //public Doctor() { }
+        public Doctor(string name, string familyName, int age) 
+            :this(new Person { Name = name, Family = familyName, Age= age})
+        {
+            
+        }
+        public Person Person { get; private set; }
+
+        public Specialities Speciality { get; set; }
+        public void ChangeName(string newName) 
+        {
+            if (newName.Length < 50)
+                Person.Name = newName;
+        }
+       
+        public override string ToString()
+        {
+            //return base.ToString();
+            return "I am doctor " +Person.Name + " " + Person.Family + " and " + Person.Age.ToString() + " years old, my specilality is " + Speciality;
         }
     }
 }
